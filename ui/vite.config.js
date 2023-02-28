888ad8b1fb6353584a21c471d3aa797edf06915a
Rodrigo Diaz <r.diazfernandez@qmul.ac.uk>
2023-02-28 17:16:03 +0100

    fix packing website as BinaryData

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import { viteSingleFile } from "vite-plugin-singlefile"
 // https://vitejs.dev/config/
+
+// We use the viteSingleFile plugin to bundle the UI into a single file
+// from https://stackoverflow.com/questions/67031295/how-to-open-a-static-website-in-localhost-but-generated-with-vite-and-without-ru
 export default defineConfig({ 
     server: {
         port: 3000,
     },
-    plugins: [react()]
+    plugins: [react(), viteSingleFile()],
 })
