0b8df88607eda146d3505744ac904a3085644277
lewiswolf <lewiswolstanholme@gmail.com>
2023-03-01 18:58:00 +0000

    vite typescript conversion without fixes

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { viteSingleFile } from "vite-plugin-singlefile"
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // We use the viteSingleFile plugin to bundle the UI into a single file
 // from https://stackoverflow.com/questions/67031295/how-to-open-a-static-website-in-localhost-but-generated-with-vite-and-without-ru
-export default defineConfig({ 
+export default defineConfig({
     server: {
         port: 3000,
     },
