071f45fdd77e231f49a2f156a61fd4031c6e839e
lewiswolf <lewiswolstanholme@gmail.com>
2023-03-01 20:54:00 +0000

    switched to index.tsx and fixed peer dependency issue

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -4,9 +4,8 @@ import { createRoot } from 'react-dom/client'
 
 // src
 import App from './components/App'
-import './css/index.css'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
