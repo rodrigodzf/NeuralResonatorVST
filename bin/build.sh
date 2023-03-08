ac037229191a5c7f019fbc4fb37fbfe495eb563b
lewiswolf <lewiswolstanholme@gmail.com>
2023-03-08 16:20:12 +0000

    update and build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+# this is a simple build script that can be used to test or build the project.
+# use --test to build the project in debug mode.
+
+# default build commands
+cmake_args="-DCMAKE_BUILD_TYPE=Release -DBROWSER_DEV_SERVER=0"
+# build commands from arguments
+for i in "$@"; do
+	case $i in
+	--test|-t)
+		cmake_args="-DCMAKE_BUILD_TYPE=Debug -DBROWSER_DEV_SERVER=1"
+		;;
+	*)
+		;;
+	esac
+done
 
 # download libtorch if it doesn't exist already
 if ! [[ -d "./third_party/libtorch" ]]
@@ -7,8 +22,10 @@ then
 		curl -L https://download.pytorch.org/libtorch/cpu/libtorch-macos-1.13.1.zip -o libtorch.zip
 		unzip libtorch.zip
 		rm -rf libtorch.zip
-	cd ../
+	cd ../	
 fi
+# add libtorch to config
+cmake_args="${cmake_args} -DCMAKE_PREFIX_PATH=./third_party/libtorch"
 
 # make build dir if it doesn't exist
 if ! [[ -d "./build" ]]
@@ -17,7 +34,5 @@ then
 fi
 
 # cmake and make
-cmake -S . -B ./build -DCMAKE_PREFIX_PATH=./third_party/libtorch -DCMAKE_BUILD_TYPE=Release -DBROWSER_DEV_SERVER=1
-cd build
-	make -j
-cd ../
+cmake -S . -B ./build $cmake_args
+cmake --build build -j
