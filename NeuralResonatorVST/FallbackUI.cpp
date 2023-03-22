abb500e441cf2711296d503c47e1438343566da2
Rodrigo Diaz <r.diazfernandez@qmul.ac.uk>
2023-03-22 13:28:47 +0100

    make the simple UI nicer

---
+++ NeuralResonatorVST/FallbackUI.cpp
@@ -0,0 +1,322 @@
+#include "FallbackUI.h"
+#include "HelperFunctions.h"
+
+ShapeComponent::ShapeComponent(juce::AudioProcessorValueTreeState &VTS)
+    : mVts(VTS)
+{
+    setOpaque(true);
+    setInterceptsMouseClicks(false, true);
+    valueTreeRedirected(VTS.state);
+    mVts.state.addListener(this);
+
+    // add a single vertex for the excitation
+    mExciter =
+        std::make_unique<ExciterVertex>(juce::Point<float>(0.5f, 0.5f));
+    addAndMakeVisible(mExciter.get());
+
+    // build the transform to go from [-1, 1] to [0, 1] with y going down
+    mMainTransform =
+        juce::AffineTransform().scaled(0.5f, 0.5f).translated(0.5f, 0.5f);
+}
+
+ShapeComponent::~ShapeComponent()
+{
+    mVts.state.removeListener(this);
+    mPath.clear();
+}
+
+void ShapeComponent::handleCommandMessage(int commandId)
+{
+    // update the tree
+    if (commandId == 0)
+    {
+        auto polygonTree = mVts.state.getChildWithName("polygon");
+
+        juce::Array<juce::var> vertices;
+
+        for (auto *vertex : mVertices)
+        {
+            vertices.add(juce::var(vertex->mRelativePos.x * 2.0f - 1));
+            vertices.add(juce::var((1 - vertex->mRelativePos.y) * 2.0f - 1));
+        }
+
+        polygonTree.setProperty("value", vertices, nullptr);
+    }
+    else if (commandId == 1)
+    {
+        JLOG("MOUSE UP IN SHAPE COMPONENT");
+        // update the tree
+        auto xpos = mVts.state.getChildWithProperty("id", "xpos");
+        auto ypos = mVts.state.getChildWithProperty("id", "ypos");
+
+        xpos.setProperty(
+            "value",
+            juce::var(mExciter->mRelativePos.x * 2.0f - 1),
+            nullptr
+        );
+        ypos.setProperty(
+            "value",
+            juce::var((1 - mExciter->mRelativePos.y) * 2.0f - 1),
+            nullptr
+        );
+    }
+}
+
+void ShapeComponent::resized()
+{
+    auto margin = getWidth() - getHeight();
+
+    mScreenTransform = juce::AffineTransform()
+                           .scaled(getHeight(), getHeight())
+                           .translated(static_cast<float>(margin) / 2.0f, 0);
+
+    mExciter->setCentrePosition(
+        mExciter->mRelativePos.transformedBy(mScreenTransform).roundToInt()
+    );
+}
+
+void ShapeComponent::paint(juce::Graphics &g)
+{
+    const juce::SpinLock::ScopedLockType lock(mMutex);
+
+    // colors should have ff in front for alpha
+    g.fillAll(juce::Colour::fromString("ff0057b8"));
+    g.setColour(juce::Colour::fromString("ffffcc00"));
+
+    for (auto *vertex : mVertices)
+        vertex->setCentrePosition(
+            vertex->mRelativePos.transformedBy(mScreenTransform).roundToInt()
+        );
+
+    // copy the path and apply a transform
+    // we need to do this because paint might be run multiple times and
+    // the shape will only get bigger otherwise
+    auto path = mPath;
+    path.applyTransform(mScreenTransform);
+    g.fillPath(path);
+}
+
+void ShapeComponent::valueTreeRedirected(juce::ValueTree &redirectedTree)
+{
+    JLOG("polygon valueTreeRedirected");
+    const juce::SpinLock::ScopedLockType lock(mMutex);
+
+    auto child = redirectedTree.getChildWithProperty("id", "vertices");
+
+    if (auto *newValue = child.getPropertyPointer("value"))
+    {
+        auto size = newValue->size();
+
+        mPath.clear();
+        mVertices.clear();
+
+        for (int i = 0; i < size; i += 2)
+        {
+            auto x = float((*newValue)[i]);
+            auto y = float((*newValue)[i + 1]);
+
+            // the positions are in the range [-1, 1], so we need
+            // to scale them to the range [0, res] and flip the y
+            // axis
+            x = (x + 1) * 0.5;
+            y = 1 - ((y + 1) * 0.5);
+            if (i == 0) { mPath.startNewSubPath(x, y); }
+            else { mPath.lineTo(x, y); }
+
+            addAndMakeVisible(mVertices.add(new DragableVertex({x, y})));
+        }
+
+        // close the subpath
+        mPath.closeSubPath();
+        repaint();
+    }
+}
+
+void ShapeComponent::valueTreePropertyChanged(
+    juce::ValueTree &changedTree,
+    const juce::Identifier &changedProperty
+)
+{
+    const juce::SpinLock::ScopedLockType lock(mMutex);
+
+    auto treeType = changedTree.getType().toString();
+
+    if (treeType == "polygon")
+    {
+        JLOG("polygon changed");
+        if (auto *flattenedVertices =
+                changedTree.getPropertyPointer(changedProperty))
+        {
+            auto size = flattenedVertices->size();
+
+            mPath.clear();
+            mVertices.clear();
+
+            for (int i = 0; i < size; i += 2)
+            {
+                auto x = float((*flattenedVertices)[i]);
+                auto y = float((*flattenedVertices)[i + 1]);
+
+                // the positions are in the range [-1, 1], so we need
+                // to scale them to the range [0, res] and flip the y
+                // axis
+                x = (x + 1) * 0.5;
+                y = 1 - ((y + 1) * 0.5);
+                if (i == 0) { mPath.startNewSubPath(x, y); }
+                else { mPath.lineTo(x, y); }
+
+                addAndMakeVisible(mVertices.add(new DragableVertex({x, y})));
+            }
+
+            // close the subpath
+            mPath.closeSubPath();
+            repaint();
+        }
+    }
+}
+
+///////////////////////////////////
+///////////////////////////////////
+///////////////////////////////////
+
+Panel::Panel(juce::AudioProcessorValueTreeState &VTS)
+    : mVTSRef(VTS)
+    , mDensityAttachment(VTS, "density", mDensitySlider)
+    , mStiffnessAttachment(VTS, "stiffness", mStiffnessSlider)
+    , mPRatioAttachment(VTS, "pratio", mPRatioSlider)
+    , mAlphaAttachment(VTS, "alpha", mAlphaSlider)
+    , mBetaAttachment(VTS, "beta", mBetaSlider)
+    , mNumVerticesSlider("Number of Vertices")
+    , mNewShapeButton("New Shape")
+{
+
+    // add the sliders with labels
+    addAndMakeVisible(mDensitySlider);
+
+    addAndMakeVisible(mDensityLabel);
+    mDensityLabel.setText("Density", juce::dontSendNotification);
+    mDensityLabel.attachToComponent(&mDensitySlider, true);
+
+    addAndMakeVisible(mStiffnessSlider);
+
+    addAndMakeVisible(mStiffnessLabel);
+    mStiffnessLabel.setText("Stiffness", juce::dontSendNotification);
+    mStiffnessLabel.attachToComponent(&mStiffnessSlider, true);
+
+    addAndMakeVisible(mPRatioSlider);
+
+    addAndMakeVisible(mPRatioLabel);
+    mPRatioLabel.setText("Poisson Ratio", juce::dontSendNotification);
+    mPRatioLabel.attachToComponent(&mPRatioSlider, true);
+
+    addAndMakeVisible(mAlphaSlider);
+
+    addAndMakeVisible(mAlphaLabel);
+    mAlphaLabel.setText("Alpha", juce::dontSendNotification);
+    mAlphaLabel.attachToComponent(&mAlphaSlider, true);
+
+    addAndMakeVisible(mBetaSlider);
+
+    addAndMakeVisible(mBetaLabel);
+    mBetaLabel.setText("Beta", juce::dontSendNotification);
+    mBetaLabel.attachToComponent(&mBetaSlider, true);
+
+    mNumVerticesSlider.setSliderStyle(juce::Slider::SliderStyle::IncDecButtons
+    );
+    mNumVerticesSlider.setIncDecButtonsMode(
+        juce::Slider::IncDecButtonMode::incDecButtonsDraggable_Vertical
+    );
+
+    mNumVerticesSlider.setRange(3, 50, 1);
+    mNumVerticesSlider.setValue(10);
+    // the number of vertices slider accepts only integers
+    mNumVerticesSlider.setNumDecimalPlacesToDisplay(0);
+
+    mNumVerticesSlider.setTextBoxStyle(
+        juce::Slider::TextEntryBoxPosition::TextBoxBelow,
+        false,
+        120,
+        20
+    );
+
+    addAndMakeVisible(mNumVerticesSlider);
+
+    // add the button
+    mNewShapeButton.onClick = [this]()
+    {
+        auto polygonTree =
+            mVTSRef.state.getOrCreateChildWithName("polygon", nullptr);
+        auto polygon = kac_core::geometry::normalisePolygon(
+            kac_core::geometry::generateConvexPolygon(
+                static_cast<unsigned long>(mNumVerticesSlider.getValue())
+            )
+        );
+
+        juce::Array<juce::var> vertices;
+
+        for (int i = 0; i < polygon.size(); ++i)
+        {
+            vertices.add(juce::var((polygon[i].x * 2.0f) - 1.));
+            vertices.add(juce::var((polygon[i].y * 2.0f) - 1.));
+        }
+
+        polygonTree.setProperty("value", vertices, nullptr);
+    };
+    addAndMakeVisible(mNewShapeButton);
+}
+
+void Panel::resized()
+{
+    auto area = getLocalBounds();
+    auto sliderHeight = area.getHeight() / 6;
+
+    // leave some space on the left for the label
+    int labelWidth = 50;
+    // set the bounds of the sliders
+    mDensitySlider.setBounds(
+        labelWidth,
+        0 * sliderHeight,
+        area.getWidth() - labelWidth,
+        sliderHeight
+    );
+    mStiffnessSlider.setBounds(
+        labelWidth,
+        1 * sliderHeight,
+        area.getWidth() - labelWidth,
+        sliderHeight
+    );
+    mPRatioSlider.setBounds(
+        labelWidth,
+        2 * sliderHeight,
+        area.getWidth() - labelWidth,
+        sliderHeight
+    );
+    mAlphaSlider.setBounds(
+        labelWidth,
+        3 * sliderHeight,
+        area.getWidth() - labelWidth,
+        sliderHeight
+    );
+    mBetaSlider.setBounds(
+        labelWidth,
+        4 * sliderHeight,
+        area.getWidth() - labelWidth,
+        sliderHeight
+    );
+
+    // mStiffnessSlider.setBounds(area.removeFromTop(sliderHeight).reduced(10));
+    // mPRatioSlider.setBounds(area.removeFromTop(sliderHeight).reduced(10));
+    // mAlphaSlider.setBounds(area.removeFromTop(sliderHeight).reduced(10));
+    // mBetaSlider.setBounds(area.removeFromTop(sliderHeight).reduced(10));
+
+    // the new shape button and the number of vertices slider are
+    // on the same row
+    mNumVerticesSlider
+        .setBounds(0, 5 * sliderHeight, area.getWidth() / 2, sliderHeight);
+    mNewShapeButton.setBounds(
+        area.getWidth() / 2,
+        5 * sliderHeight,
+        area.getWidth() / 2,
+        sliderHeight
+    );
+}
--- NeuralResonatorVST/FallbackUI.h
+++ NeuralResonatorVST/FallbackUI.h
@@ -6,124 +6,67 @@
 #include <geometry/generate_polygon.hpp>
 #include <geometry/morphisms.hpp>
 #include <geometry/lines.hpp>
+#include <memory>
+#include <vector>
+#include "DragableVertex.h"
+
+class ExciterVertex : public DragableVertex
+{
+public:
+    ExciterVertex(const juce::Point<float>& initialPos)
+        : DragableVertex(initialPos)
+    {
+    }
+
+    void paint(juce::Graphics& g) override
+    {
+        g.setColour(juce::Colours::red.withAlpha(
+            isMouseOverOrDragging() ? 0.9f : 0.5f
+        ));
+        g.fillEllipse(getLocalBounds().reduced(3).toFloat());
+
+        g.setColour(juce::Colours::red);
+        g.drawEllipse(getLocalBounds().reduced(3).toFloat(), 2.0f);
+    }
+
+    void mouseUp(const juce::MouseEvent& e) override
+    {
+        JLOG("MOUSE UP");
+        // send a message to the parent component
+        getParentComponent()->postCommandMessage(1);
+    }
+};
+
 class ShapeComponent : public juce::Component,
                        public juce::ValueTree::Listener
 {
 public:
-    ShapeComponent(juce::AudioProcessorValueTreeState &VTS)
-        : mVts(VTS)
-    {
-        setOpaque(true);
-        valueTreeRedirected(VTS.state);
-        mVts.state.addListener(this);
-        // setInterceptsMouseClicks(false, false);
-    }
+    ShapeComponent(juce::AudioProcessorValueTreeState& VTS);
 
-    ~ShapeComponent()
-    {
-        mVts.state.removeListener(this);
-        mPath.clear();
-    }
+    ~ShapeComponent();
 
-    void paint(juce::Graphics &g) override
-    {
-        const juce::SpinLock::ScopedLockType lock(mMutex);
+    void handleCommandMessage(int commandId) override;
 
-        // colors should have ff in front for alpha
-        g.fillAll(juce::Colour::fromString("ff0057b8"));
-        g.setColour(juce::Colour::fromString("ffffcc00"));
+    void resized() override;
 
-        auto margin = getWidth() - getHeight();
-        auto transform =
-            juce::AffineTransform()
-                .scaled(getHeight(), getHeight())
-                .translated(static_cast<float>(margin) / 2.0f, 0);
+    void paint(juce::Graphics& g) override;
 
-        // copy the path and apply a transform
-        // we need to do this because paint might be run multiple times and
-        // the shape will only get bigger otherwise
-        auto path = mPath;
-        path.applyTransform(transform);
-        g.fillPath(path);
-    }
-
-    void valueTreeRedirected(juce::ValueTree &redirectedTree) override
-    {
-        JLOG("polygon valueTreeRedirected");
-        const juce::SpinLock::ScopedLockType lock(mMutex);
-
-        auto child = redirectedTree.getChildWithProperty("id", "vertices");
-
-        if (auto *newValue = child.getPropertyPointer("value"))
-        {
-            auto size = newValue->size();
-
-            mPath.clear();
-
-            for (int i = 0; i < size; i += 2)
-            {
-                auto x = float((*newValue)[i]);
-                auto y = float((*newValue)[i + 1]);
-
-                // the positions are in the range [-1, 1], so we need
-                // to scale them to the range [0, res] and flip the y
-                // axis
-                x = (x + 1) * 0.5;
-                y = 1 - ((y + 1) * 0.5);
-                if (i == 0) { mPath.startNewSubPath(x, y); }
-                else { mPath.lineTo(x, y); }
-            }
-
-            // close the subpath
-            mPath.closeSubPath();
-            repaint();
-        }
-    }
+    void valueTreeRedirected(juce::ValueTree& redirectedTree) override;
 
     void valueTreePropertyChanged(
-        juce::ValueTree &changedTree,
-        const juce::Identifier &changedProperty
-    ) override
-    {
-        const juce::SpinLock::ScopedLockType lock(mMutex);
-
-        auto treeType = changedTree.getType().toString();
-
-        if (treeType == "polygon")
-        {
-            JLOG("polygon changed");
-            if (auto *flattenedVertices =
-                    changedTree.getPropertyPointer(changedProperty))
-            {
-                auto size = flattenedVertices->size();
-
-                mPath.clear();
-
-                for (int i = 0; i < size; i += 2)
-                {
-                    auto x = float((*flattenedVertices)[i]);
-                    auto y = float((*flattenedVertices)[i + 1]);
-
-                    // the positions are in the range [-1, 1], so we need
-                    // to scale them to the range [0, res] and flip the y
-                    // axis
-                    x = (x + 1) * 0.5;
-                    y = 1 - ((y + 1) * 0.5);
-                    if (i == 0) { mPath.startNewSubPath(x, y); }
-                    else { mPath.lineTo(x, y); }
-                }
-
-                // close the subpath
-                mPath.closeSubPath();
-                repaint();
-            }
-        }
-    }
+        juce::ValueTree& changedTree,
+        const juce::Identifier& changedProperty
+    ) override;
 
 private:
     juce::Path mPath;
     juce::SpinLock mMutex;
-    juce::AudioProcessorValueTreeState &mVts;
+    juce::AudioProcessorValueTreeState& mVts;
+    juce::OwnedArray<DragableVertex> mVertices;
+    std::unique_ptr<ExciterVertex> mExciter;
+
+    juce::AffineTransform mMainTransform;
+    juce::AffineTransform mScreenTransform;
 
 private:
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ShapeComponent)
@@ -132,75 +75,22 @@ private:
 class Panel : public juce::Component
 {
 public:
-    Panel(juce::AudioProcessorValueTreeState &VTS)
-        : mVTSRef(VTS)
-        , mDensityAttachment(VTS, "density", mDensitySlider)
-        , mStiffnessAttachment(VTS, "stiffness", mStiffnessSlider)
-        , mPRatioAttachment(VTS, "pratio", mPRatioSlider)
-        , mAlphaAttachment(VTS, "alpha", mAlphaSlider)
-        , mBetaAttachment(VTS, "beta", mBetaSlider)
-        , mNewShapeButton("New Shape")
-    {
-        // add the sliders
-        addAndMakeVisible(mDensitySlider);
-        addAndMakeVisible(mStiffnessSlider);
-        addAndMakeVisible(mPRatioSlider);
-        addAndMakeVisible(mAlphaSlider);
-        addAndMakeVisible(mBetaSlider);
-
-        // add the button
-        mNewShapeButton.onClick = [this]()
-        {
-            auto polygonTree =
-                mVTSRef.state.getOrCreateChildWithName("polygon", nullptr);
-            auto polygon = kac_core::geometry::normalisePolygon(
-                kac_core::geometry::generateConvexPolygon(10)
-            );
-            // kac_core::geometry::generateIrregularStar(float(shape["value"])
-            // kac_core::geometry::generatePolygon(float(shape["value"])
-
-            juce::Array<juce::var> vertices;
-
-            for (int i = 0; i < polygon.size(); ++i)
-            {
-                vertices.add(juce::var((polygon[i].x * 2.0f) - 1.));
-                vertices.add(juce::var((polygon[i].y * 2.0f) - 1.));
-            }
-
-            polygonTree.setProperty("value", vertices, nullptr);
-        };
-        addAndMakeVisible(mNewShapeButton);
-
-        // setOpaque(true);
-        // setInterceptsMouseClicks(false, false);
-    }
-
-    void paint(juce::Graphics &g) override
-    {
-        // g.fillAll(juce::Colours::lightgrey);
-    }
-
-    void resized() override
-    {
-        auto area = getLocalBounds();
-        auto sliderHeight = area.getHeight() / 6;
-        mDensitySlider.setBounds(area.removeFromTop(sliderHeight));
-        mStiffnessSlider.setBounds(area.removeFromTop(sliderHeight));
-        mPRatioSlider.setBounds(area.removeFromTop(sliderHeight));
-        mAlphaSlider.setBounds(area.removeFromTop(sliderHeight));
-        mBetaSlider.setBounds(area.removeFromTop(sliderHeight));
-        mNewShapeButton.setBounds(area.removeFromTop(sliderHeight));
-    }
+    Panel(juce::AudioProcessorValueTreeState& VTS);
+    void resized() override;
 
 private:
-    juce::AudioProcessorValueTreeState &mVTSRef;
+    juce::AudioProcessorValueTreeState& mVTSRef;
     juce::Slider mDensitySlider, mStiffnessSlider, mPRatioSlider,
         mAlphaSlider, mBetaSlider;
+
+    juce::Label mDensityLabel, mStiffnessLabel, mPRatioLabel, mAlphaLabel,
+        mBetaLabel;
 
     juce::AudioProcessorValueTreeState::SliderAttachment mDensityAttachment,
         mStiffnessAttachment, mPRatioAttachment, mAlphaAttachment,
         mBetaAttachment;
 
+    juce::Slider mNumVerticesSlider;
     juce::TextButton mNewShapeButton;
 
 private:
